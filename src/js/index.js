f05efcf19a15f34851dafad3711b12ecd640a47b
caamomilaa <c.velazquezdamico@gmail.com>
2024-07-01 21:49:08 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,19 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
 
 let userPoints = 0;
 let pcPoints = 0;
 let userSelection;
 let pcSelection;
-const gameContainerElement = document.getElementById('game-container');
+const gameContainerElement = document.getElementById("game-container");
 
-const setUserSelection = event => {};
+const handleClick = (event) => {
+  console.log(event.target);
+};
+gameContainerElement.addEventListener("click", handleClick);
+
+//falta lo de pointer events porque da click en cualquier lado esat cosa.
+
+//const setUserSelection = (event) => {};
+
+const setPCSelection = () => {};
