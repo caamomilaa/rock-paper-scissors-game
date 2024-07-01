1d579a9e0b5469fb0509d5bd91da69bcd80bcf7c
Ucmessina <163582100+caamomilaa@users.noreply.github.com>
2024-07-01 18:44:01 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+
+let userPoints = 0;
+let pcPoints = 0;
+let userSelection;
+let pcSelection;
+const gameContainerElement = document.getElementById('game-container');
+
+const setUserSelection = event => {};
